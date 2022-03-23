8b9e74c2720bd61ac0de2dc75a80a1de9eb7fdd0
mayara merlin <mayaramerlin@hotmail.com>
2022-03-23 20:45:47 -0300

    FIRST COMMIT

---
+++ k8s/server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8000", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprint(w, "Olá, Eu sou %s. I'm %s", name, age)
+}
